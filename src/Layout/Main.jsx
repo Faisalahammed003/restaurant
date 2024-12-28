c9362ccde14544d515370a823cb4f4008b6de6f7
/Faisalahammed003 <faisalahammed5035@gmail.com>
2024-12-29 03:58:20 +0600

    banner-completed

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Pages/Share/Footer";
+import Nav from "./Pages/Share/Nav";
+
+const Main = () => {
+  return (
+    <div>
+      <Nav></Nav>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Layout/Main";
+import Home from "./Layout/Pages/Home/Home";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>
 );
