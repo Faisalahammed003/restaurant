b5900fda9011043eaeb098293be10137e6cb58be
/Faisalahammed003 <faisalahammed5035@gmail.com>
2024-12-31 17:54:13 +0600

    menu-cover

--- src/Layout/Pages/Menu/Menu.jsx
+++ src/Layout/Pages/Menu/Menu.jsx
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Share/Cover";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import MenuCover from "../Share/MenuCover";
+import img from "../../../assets/home/chef-service.jpg";
 
 const Menu = () => {
   return (
@@ -9,6 +11,7 @@ const Menu = () => {
         <title>Bistro | Menu</title>
       </Helmet>
       <Cover img={menuImg} title="our menu"></Cover>
+      <MenuCover img={img} title="Desserts"></MenuCover>
     </div>
   );
 };
--- src/Layout/Pages/Share/MenuCover.jsx
+++ src/Layout/Pages/Share/MenuCover.jsx
@@ -1,11 +1,9 @@
-import img from "../../../assets/home/chef-service.jpg";
-
-const BistroBoss = () => {
+const MenuCover = ({ img, title }) => {
   return (
     <div className=" mb-24 relative">
       <img className="h-32 md:h-80 lg:h-96 w-full" src={img} alt="" />
-      <div className="text-center mx-auto px-2 py-4 md:px-10 md:py-20 text-black bg-white max-w-[763px] absolute -top-1 lg:top-24 lg:right-64 md:top-10 md:right-20 right-17">
-        <h3 className="text-xl md:text-2xl uppercase">Bistro Boss</h3>
+      <div className="text-center mx-auto px-2 py-4 md:px-10 md:py-20 text-white bg-[#1111116e] max-w-[763px] absolute -top-1 lg:top-24 lg:right-64 md:top-10 md:right-20 right-17">
+        <h3 className="text-xl md:text-2xl uppercase">{title}</h3>
         <p className="text-sm">
           Lorem Ipsum has been the industry’s standard dummy text ever since the
           1500s, when an unknown printer took a galley of type and scrambled it
@@ -16,4 +14,4 @@ const BistroBoss = () => {
   );
 };
 
-export default BistroBoss;
+export default MenuCover;
